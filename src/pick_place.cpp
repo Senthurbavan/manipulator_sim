6429731999d11e6ac8d921e65b2349076eea4af3
Senthurbavan <senthurbavankiruba@gmail.com>
2024-02-19 19:06:17 -0500

    indent 4 space to 2 space

--- src/pick_place.cpp
+++ src/pick_place.cpp
@@ -99,97 +99,97 @@ void place(moveit::planning_interface::MoveGroupInterface& group)
 
 void setupEnvironmentObjects(moveit::planning_interface::PlanningSceneInterface& psi)
 {
-    std::vector<moveit_msgs::CollisionObject> collision_objects;
-    collision_objects.resize(3);
+  std::vector<moveit_msgs::CollisionObject> collision_objects;
+  collision_objects.resize(3);
 
-    collision_objects[0].id = "table1";
-    collision_objects[0].header.frame_id = "panda_link0";
+  collision_objects[0].id = "table1";
+  collision_objects[0].header.frame_id = "panda_link0";
 
-    collision_objects[0].primitives.resize(1);
-    collision_objects[0].primitives[0].type = collision_objects[0].primitives[0].BOX;
-    collision_objects[0].primitives[0].dimensions.resize(3);
-    collision_objects[0].primitives[0].dimensions[0] = 0.2;
-    collision_objects[0].primitives[0].dimensions[1] = 0.4;
-    collision_objects[0].primitives[0].dimensions[2] = 0.4; 
+  collision_objects[0].primitives.resize(1);
+  collision_objects[0].primitives[0].type = collision_objects[0].primitives[0].BOX;
+  collision_objects[0].primitives[0].dimensions.resize(3);
+  collision_objects[0].primitives[0].dimensions[0] = 0.2;
+  collision_objects[0].primitives[0].dimensions[1] = 0.4;
+  collision_objects[0].primitives[0].dimensions[2] = 0.4; 
 
-    collision_objects[0].primitive_poses.resize(1);
-    collision_objects[0].primitive_poses[0].position.x = 0.5;
-    collision_objects[0].primitive_poses[0].position.y = 0;
-    collision_objects[0].primitive_poses[0].position.z = 0.2;
-    collision_objects[0].primitive_poses[0].orientation.w = 1.0;
+  collision_objects[0].primitive_poses.resize(1);
+  collision_objects[0].primitive_poses[0].position.x = 0.5;
+  collision_objects[0].primitive_poses[0].position.y = 0;
+  collision_objects[0].primitive_poses[0].position.z = 0.2;
+  collision_objects[0].primitive_poses[0].orientation.w = 1.0;
 
-    collision_objects[0].operation = collision_objects[0].ADD;
+  collision_objects[0].operation = collision_objects[0].ADD;
 
 
-    collision_objects[1].id = "table2";
-    collision_objects[1].header.frame_id = "panda_link0";
+  collision_objects[1].id = "table2";
+  collision_objects[1].header.frame_id = "panda_link0";
 
-    collision_objects[1].primitives.resize(1);
-    collision_objects[1].primitives[0].type = collision_objects[0].primitives[0].BOX;
-    collision_objects[1].primitives[0].dimensions.resize(3);
-    collision_objects[1].primitives[0].dimensions[0] = 0.4;
-    collision_objects[1].primitives[0].dimensions[1] = 0.2;
-    collision_objects[1].primitives[0].dimensions[2] = 0.4; 
+  collision_objects[1].primitives.resize(1);
+  collision_objects[1].primitives[0].type = collision_objects[0].primitives[0].BOX;
+  collision_objects[1].primitives[0].dimensions.resize(3);
+  collision_objects[1].primitives[0].dimensions[0] = 0.4;
+  collision_objects[1].primitives[0].dimensions[1] = 0.2;
+  collision_objects[1].primitives[0].dimensions[2] = 0.4; 
 
-    collision_objects[1].primitive_poses.resize(1);
-    collision_objects[1].primitive_poses[0].position.x = 0;
-    collision_objects[1].primitive_poses[0].position.y = 0.5;
-    collision_objects[1].primitive_poses[0].position.z = 0.2;
-    collision_objects[1].primitive_poses[0].orientation.w = 1.0;
+  collision_objects[1].primitive_poses.resize(1);
+  collision_objects[1].primitive_poses[0].position.x = 0;
+  collision_objects[1].primitive_poses[0].position.y = 0.5;
+  collision_objects[1].primitive_poses[0].position.z = 0.2;
+  collision_objects[1].primitive_poses[0].orientation.w = 1.0;
 
-    collision_objects[1].operation = collision_objects[1].ADD;
+  collision_objects[1].operation = collision_objects[1].ADD;
 
-    collision_objects[2].id = "object";
-    collision_objects[2].header.frame_id = "panda_link0";
+  collision_objects[2].id = "object";
+  collision_objects[2].header.frame_id = "panda_link0";
 
-    collision_objects[2].primitives.resize(1);
-    collision_objects[2].primitives[0].type = collision_objects[0].primitives[0].BOX;
-    collision_objects[2].primitives[0].dimensions.resize(3);
-    collision_objects[2].primitives[0].dimensions[0] = 0.02;
-    collision_objects[2].primitives[0].dimensions[1] = 0.02;
-    collision_objects[2].primitives[0].dimensions[2] = 0.2; 
+  collision_objects[2].primitives.resize(1);
+  collision_objects[2].primitives[0].type = collision_objects[0].primitives[0].BOX;
+  collision_objects[2].primitives[0].dimensions.resize(3);
+  collision_objects[2].primitives[0].dimensions[0] = 0.02;
+  collision_objects[2].primitives[0].dimensions[1] = 0.02;
+  collision_objects[2].primitives[0].dimensions[2] = 0.2; 
 
-    collision_objects[2].primitive_poses.resize(1);
-    collision_objects[2].primitive_poses[0].position.x = 0.5;
-    collision_objects[2].primitive_poses[0].position.y = 0;
-    collision_objects[2].primitive_poses[0].position.z = 0.5;
-    collision_objects[2].primitive_poses[0].orientation.w = 1.0;
+  collision_objects[2].primitive_poses.resize(1);
+  collision_objects[2].primitive_poses[0].position.x = 0.5;
+  collision_objects[2].primitive_poses[0].position.y = 0;
+  collision_objects[2].primitive_poses[0].position.z = 0.5;
+  collision_objects[2].primitive_poses[0].orientation.w = 1.0;
 
-    collision_objects[2].operation = collision_objects[2].ADD;
+  collision_objects[2].operation = collision_objects[2].ADD;
 
-    psi.addCollisionObjects(collision_objects);
+  psi.addCollisionObjects(collision_objects);
 
 }
 
 
 int main(int argc, char** argv){
-    ros::init(argc, argv, "pick_place");
-    ros::NodeHandle nh;
-    ros::AsyncSpinner spinner(1);
-    spinner.start();
+  ros::init(argc, argv, "pick_place");
+  ros::NodeHandle nh;
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
 
-    ROS_INFO("Pick-Place Initializing");
+  ROS_INFO("Pick-Place Initializing");
 
-    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-    moveit::planning_interface::MoveGroupInterface group_interface("panda_arm");
-    group_interface.setPlanningTime(45.0);
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+  moveit::planning_interface::MoveGroupInterface group_interface("panda_arm");
+  group_interface.setPlanningTime(45.0);
 
-    // For visualization
-    namespace rvt = rviz_visual_tools;
-    moveit_visual_tools::MoveItVisualTools visual_tools("panda_link0");
-    visual_tools.deleteAllMarkers();
-    visual_tools.loadRemoteControl();
+  // For visualization
+  namespace rvt = rviz_visual_tools;
+  moveit_visual_tools::MoveItVisualTools visual_tools("panda_link0");
+  visual_tools.deleteAllMarkers();
+  visual_tools.loadRemoteControl();
 
-    //add collision objects
-    setupEnvironmentObjects(planning_scene_interface);
-    
-    // Prompt to start
-    visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start");
+  //add collision objects
+  setupEnvironmentObjects(planning_scene_interface);
+  
+  // Prompt to start
+  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start");
 
-    pick(group_interface);  
-    visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue");
-    place(group_interface);
+  pick(group_interface);  
+  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue");
+  place(group_interface);
 
-    ros::waitForShutdown();
-    return 0;
+  ros::waitForShutdown();
+  return 0;
 }
